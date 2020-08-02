181d141c6e8913445f4395612824c861dc50105c
Thigpen <mthigpen@amazon.com>
2020-08-02 18:37:54 -0500

    Add project files.

---
+++ Activity2/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Activity1Part2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "Test",
+              url: "Test/{action}",
+              defaults: new { controller = "Test", action = "Index" }
+          );
+        }
+    }
+}
